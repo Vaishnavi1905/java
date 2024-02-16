5492f1ae81765ca64c0ddba7da68137e3b412013
Vaishnavi1905 <153627393+Vaishnavi1905@users.noreply.github.com>
2024-02-16 14:19:56 +0530

    Add files via upload

---
+++ SumOfNaturalNo.java
@@ -0,0 +1,21 @@
+class SumOfNaturalNo
+{
+public static void main(String []args)
+{
+int value=20;
+int sum=(value*(value+1))/2;
+boolean result=sum%2==0;
+System.out.println("The sum of natural numbers:"+sum);
+System.out.println("The result for sum of natural numbers:"+result);
+}}
+
+
+
+/*
+Output:
+1.Compile Time:Success
+2.Runtime:Success
+The sum of natural numbers:210
+The result for sum of natural numbers:true
+*/
+
