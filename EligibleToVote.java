1e6fcef01466750d8e48f2f2bedcebd5bbe4760f
Vaishnavi1905 <153627393+Vaishnavi1905@users.noreply.github.com>
2024-02-16 14:25:32 +0530

    Add files via upload

---
+++ EligibleToVote.java
@@ -0,0 +1,15 @@
+class EligibleToVote
+{
+public static void main(String[] args)
+{
+int age=20;
+boolean eligible_age=age>=18;
+System.out.println("Can I vote:"+eligible_age);
+}}
+
+
+/*
+1.Compile time:Success
+2.Runtime:Success
+Can I vote:true
+*/
