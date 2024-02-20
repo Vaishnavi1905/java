3f5e54ee0c4d5c446b5dbaa27ecfb968f1561e5e
Vaishnavi Deshmane <153627393+Vaishnavi1905@users.noreply.github.com>
2024-02-20 23:12:14 +0530

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PrintName
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String name=sc.nextLine();
+System.out.println(name);
+}
+}
+
+
+/*
+Output:
+1.Compile Time:Success
+2.Runtime:Success
+My name is Vaishnavi
+My name is Vaishnavi
+8
+*/
