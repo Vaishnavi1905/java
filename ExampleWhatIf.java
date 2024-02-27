74f4c036d2266f9850cf6a1e815307cd4802f03f
Vaishnavi Deshmane <153627393+Vaishnavi1905@users.noreply.github.com>
2024-02-27 14:18:36 +0530

    Add files via upload

---
+++ ExampleWhatIf.java
@@ -0,0 +1,18 @@
+class ExampleWhatIf
+{
+   public static void main(String[] args)
+   {
+   int age=20;
+     if(age>=18)
+     {
+       System.out.println("You are eligible to vote"); //If the condition become false it will return nothing
+     }
+   }
+}
+
+/*
+Output:
+1.Compile Time:Success
+2.Runtime:Success
+You are eligible to vote
+*/
