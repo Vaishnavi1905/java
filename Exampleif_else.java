ad93f6ab8f2d36c1704e355dd13c1abadf17aa67
Vaishnavi Deshmane <153627393+Vaishnavi1905@users.noreply.github.com>
2024-02-27 14:19:10 +0530

    Add files via upload

---
+++ Exampleif_else.java
@@ -0,0 +1,22 @@
+class Exampleif_else
+{
+   public static void main(String[] args)
+   {
+   int money=100;
+     if(money>=120)
+     {
+       System.out.println("You can buy ticket for a movie"); //If the condition become true it will return this line
+     }
+     else
+     {
+       System.out.println("You can not buy ticket for a movie"); //If the condition become false it will return this line
+     }
+   }
+}
+
+/*
+Output:
+1.Compile Time:Success
+2.Runtime:Success
+You can not buy ticket for a movie
+*/
