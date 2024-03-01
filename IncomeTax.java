c7e542c05a8434d779d817eedea26c5ea19610a8
Vaishnavi Deshmane <153627393+Vaishnavi1905@users.noreply.github.com>
2024-03-01 16:53:46 +0530

    Update IncomeTax.java

--- IncomeTax.java
+++ IncomeTax.java
@@ -4,17 +4,17 @@ public static void main(String []args)
 {
 double income=90000;
 double tax=0;
-if(income <=30000)
+if(income <=2.5L)
 {
 tax=(income*0.10);
 System.out.println("IncomeTax:"+tax);
 }
-else if(income <=40000)
+else if(income <=5.0L)
 {
 tax=(income*0.20);
 System.out.println("IncomeTax:"+tax);
 }
-else if(income>=50000)
+else if(income>=10L)
 {
 tax=(income*0.30);
 System.out.println("IncomeTax:"+tax);
@@ -25,6 +25,3 @@ System.out.println("Income Tax:"+tax);
 }
 }
 }
-//output
-compile time -success
-Run time-IncomeTax:27000.0
