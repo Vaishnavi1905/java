8e265a0fb7b8b3e204fa3e7d855aabe9ecb0980c
Vaishnavi Deshmane <153627393+Vaishnavi1905@users.noreply.github.com>
2024-03-02 12:00:43 +0530

    Update IncomeTax.java

--- IncomeTax.java
+++ IncomeTax.java
@@ -1,27 +1,36 @@
-class IncomeTax
-{
-public static void main(String []args)
-{
-double income=90000;
-double tax=0;
-if(income <=2.5L)
-{
-tax=(income*0.10);
-System.out.println("IncomeTax:"+tax);
+import java.util.Scanner;
+
+class IncomeTax {
+    public static void main(String[] args)
+  {
+        float income = 0.0f; // Declare income variable
+        float tax = 0.0f; // Declare tax variable
+        System.out.println("Enter the income:");
+        Scanner sc = new Scanner(System.in);
+        income = sc.nextFloat(); // Assign user input to income variable
+
+        if (income < 2.5f)
+        {
+            tax = 0.0f;
+            System.out.println("IncomeTax:" + tax);
+        } else if (income <= 5.0f) 
+        {
+            tax = (income * 0.20f);
+            System.out.println("IncomeTax:" + tax);
+        } 
+        else if (income >= 10.0f)
+        {
+            tax = (income * 0.30f);
+            System.out.println("IncomeTax:" + tax);
+        }
+        else {
+            System.out.println("Income Tax:" + tax);
+        }
+    }
 }
-else if(income <=5.0L)
-{
-tax=(income*0.20);
-System.out.println("IncomeTax:"+tax);
-}
-else if(income>=10L)
-{
-tax=(income*0.30);
-System.out.println("IncomeTax:"+tax);
-}
-else
-{
-System.out.println("Income Tax:"+tax);
-}
-}
-}
+
+
+
+
+
+
